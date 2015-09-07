353b425621a803c3d3d6b5c69816a3de9334648f
Brian Hall <brian@hallmanac.com>
2015-09-07 16:25:34 -0400

    Added implementation for IAppRuntimeContext.

---
+++ src/Hallmanac.ApplicationRuntime/AppRuntimeContext.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Concurrent;
+
+
+namespace Hallmanac.ApplicationRuntime
+{
+    public class AppRuntimeContext : IAppRuntimeContext
+    {
+        public ConcurrentDictionary<string, object> Environment { get; } = new ConcurrentDictionary<string, object>();
+
+
+        public T Get<T>(string key)
+        {
+            object value;
+            return Environment.TryGetValue(key, out value) ? (T)value : default(T);
+        }
+
+
+        public IAppRuntimeContext Set<T>(string key, T value)
+        {
+            Environment.AddOrUpdate(key, value, (s, o) => value);
+            return this;
+        }
+
+
+        public IAppRuntimeContext Remove<T>(string key)
+        {
+            object value;
+            Environment.TryRemove(key, out value);
+            return this;
+        }
+    }
+}
--- src/Hallmanac.ApplicationRuntime/IAppRuntimeContext.cs
+++ src/Hallmanac.ApplicationRuntime/IAppRuntimeContext.cs
@@ -1,17 +1,19 @@
-﻿using System.Collections.Generic;
-using System.Runtime.InteropServices.ComTypes;
+﻿using System.Collections.Concurrent;
 
 
 namespace Hallmanac.ApplicationRuntime
 {
     public interface IAppRuntimeContext
     {
-        IDictionary<string, object> Environment { get; }
+        ConcurrentDictionary<string, object> Environment { get; }
 
 
         T Get<T>(string key);
 
 
         IAppRuntimeContext Set<T>(string key, T value);
+
+
+        IAppRuntimeContext Remove<T>(string key);
     }
 }
