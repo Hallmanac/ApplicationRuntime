996785030649f2328010cfa8e5d8288013e9125b
Brian Hall <brian@hallmanac.com>
2015-09-07 14:16:50 -0400

    Renaming changes

---
+++ Hallmanac.ApplicationRuntime/IAppRuntimeController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+namespace Hallmanac.ApplicationRuntime
+{
+    public interface IAppRuntimeController
+    {
+        IAppRuntimeController AddToPipeline(Func<IAppRuntimeContext, Func<Task>, Task> pipelineFunc);
+    }
+}
