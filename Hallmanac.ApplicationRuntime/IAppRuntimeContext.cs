e03273ae3ea372799c3ed7c94312ec74353a7dba
Brian Hall <brian@hallmanac.com>
2015-09-07 14:16:50 -0400

    Renaming changes

---
+++ Hallmanac.ApplicationRuntime/IAppRuntimeContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+
+
+namespace Hallmanac.ApplicationRuntime
+{
+    public interface IAppRuntimeContext
+    {
+        IDictionary<string, object> Environment { get; }
+
+
+        T Get<T>(string key);
+
+
+        IAppRuntimeContext Set<T>(string key, T value);
+    }
+}
